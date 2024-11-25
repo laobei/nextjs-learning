f1ae773c38a16be6ab916e2363d1fd73329f8f48
tim <shaohualin@hotmail.com>
2024-11-25 17:02:09 +0800

    day 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from './ui/home.module.css'
 
 export default function Page() {
   return (
@@ -27,7 +28,9 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
         </div>
+        <div  className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"> </div>
       </div>
+      <div className={styles.shape}></div>
     </main>
   );
 }
